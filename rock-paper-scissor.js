f27670bffbfbdb3d281af94aafc1cc7a4dab64e7
Chandler Finley <112525003+ChandoFin@users.noreply.github.com>
2025-06-11 16:31:49 -0700

    finish javascript
    added all necessary functions in javascript to
    make the game work properly

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -5,9 +5,9 @@
 // the user. I will then need to output a victory or defeat message.
 
 /* Create a function that retrieves a randomly generated number up to 15
-// If that number is <= 5 pick rock
-// Else if that number is >5 && <=10 pick paper
-// Else if that number is >10 && <= 15 pick scissors
+// If that number is < .33 pick rock
+// Else if that number is > .33 < .66 pick paper
+// Else pick scissors
 
 /* Create a function that allows the user to enter either rock, paper, or scissors
 /* Create a function that places the computers choice against the users choice
@@ -15,7 +15,7 @@
 // If the user chooses rock and the computer gets scissors issue a message "you win!"
 /* If the user  chooses rock and the computer gets rock then issue a message "It's a draw!" */
 
-console.log(getComputerChoice())
+
 
 function getComputerChoice() {
     const random = Math.random();
@@ -30,3 +30,55 @@ function getComputerChoice() {
     }
 }
     
+function getHumanChoice() { 
+    return prompt("enter rock, paper, or scissors:").toLowerCase();
+    
+}
+
+
+let humanScore = 0;
+
+let computerScore = 0;
+
+function playGame() {
+
+        function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return `it's a draw! you both chose ${humanChoice}!`;
+        }
+    
+        else if (
+            (humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper') ||
+            (humanChoice === 'paper' && computerChoice === 'rock')
+        )
+            {
+                humanScore++; 
+                return `You win! ${humanChoice} beats ${computerChoice}!`;
+            }
+        
+        else if (
+            (computerChoice === 'rock' && humanChoice === 'scissors') ||
+            (computerChoice === 'paper' && humanChoice === 'rock') ||
+            (computerChoice === 'scissors' && humanChoice === 'paper')
+        )
+            {
+                computerScore++;
+                return `You lose! ${computerChoice} beats ${humanChoice}!`;
+
+            }
+        
+    }
+
+      for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const result = playRound(humanSelection, computerSelection);
+        console.log(`Round ${i+1}: ${result}`);
+    }
+    console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`);
+}
+
+
+
+console.log(playGame());
